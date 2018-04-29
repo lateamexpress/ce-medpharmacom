fbb695aa90b06f42875577b7fb6e2c47abc6d29d
Evanarch <duchassin.nicolas@gmail.com>
2018-04-29 20:21:37 +0200

    Hydratation page produit

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produit;
 
 class ProduitController extends Controller
 {
-    public function index() {
-        return view('client/produit');
+    public function index($id) {
+
+        $produit = Produit::find($id);
+
+        return view('client/produit', [
+            'produit' => $produit
+        ]);
     }
 }
