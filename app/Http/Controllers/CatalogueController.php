e4aa7f4c20cf63d2e2561e548de7eae5e5cbb121
Evanarch <duchassin.nicolas@gmail.com>
2018-07-08 22:49:08 +0200

    On travail avec Kévin sur les filtres

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -16,7 +16,7 @@ class CatalogueController extends Controller
         $catalogue = Produit::paginate(15);
         $marque = Marque::all();
         $categorie = Categorie::all();
-        $poduitLastFive = Produit::all()->sortByDesc('id_produit')->take(5);
+        $poduitLastFive = Produit::all()->sortByDesc('id_produit')->take(6);
         return view('client/catalogue',[
             'catalogue' => $catalogue,
             'produitLastFive' => $poduitLastFive,
@@ -76,8 +76,28 @@ class CatalogueController extends Controller
             return View('client/commande')->with('commande', '');
         }
         elseif ($request['Rechercher']) {
-            // Fait ta requête ou appelle là
-            
+            $nom_produit = $request->all()['nom_produit'];
+            $ref_id_marque = $request->all()['ref_id_marque'];
+            $ref_id_categorie = $request->all()['ref_id_categorie'];
+            $cout = $request->all()['cout'];
+            $marque = Marque::all();
+            $categorie = Categorie::all();
+            $poduitLastFive = Produit::all()->sortByDesc('id_produit')->take(6);
+
+
+            $catalogue = Produit::where([
+                ['nom_produit', 'like', '%'. $nom_produit .'%'],
+                ['ref_id_marque', 'like', '%'. $ref_id_marque .'%'],
+                ['ref_id_categorie', 'like', '%'. $ref_id_categorie .'%'],
+                ['cout', 'like', '%'. $cout .'%'],
+            ])->get();
+
+            return view('client/catalogue',[
+                'catalogue' => $catalogue,
+                'produitLastFive' => $poduitLastFive,
+                'marque' => $marque,
+                'categorie' => $categorie,
+            ]);
         }
         else {
             return response()->json(['success'=>"Ce produit vient d'être ajouté"]);
